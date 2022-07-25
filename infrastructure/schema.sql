511244cb0a571cd53b2e206214b426e172e852eb
Alherd <algerd.blr@gmail.com>
2022-07-25 18:21:20 +0300

    trigger for creating user profile

--- infrastructure/schema.sql
+++ infrastructure/schema.sql
@@ -2,7 +2,8 @@ CREATE OR REPLACE FUNCTION public.placeholder_feature_type(point_row public.poin
     RETURNS text
     LANGUAGE sql
     STABLE
-AS $function$
+AS
+$function$
 SELECT 'Feature'
 $function$;
 
@@ -10,7 +11,8 @@ CREATE OR REPLACE FUNCTION public.placeholder_feature_collection_type(track_row 
     RETURNS text
     LANGUAGE sql
     STABLE
-AS $function$
+AS
+$function$
 SELECT 'FeatureCollection'
 $function$;
 
@@ -18,13 +20,16 @@ CREATE OR REPLACE FUNCTION public.merged_properties(point_row public.point)
     RETURNS jsonb
     LANGUAGE sql
     STABLE
-AS $function$
-SELECT point_row.properties || jsonb_build_object('track_id', point_row.track_id, 'spectrum_id', point_row.spectrum_id, 'point_id', point_row.id)
+AS
+$function$
+SELECT point_row.properties ||
+       jsonb_build_object('track_id', point_row.track_id, 'spectrum_id', point_row.spectrum_id, 'point_id',
+                          point_row.id)
 $function$;
 
-DROP VIEW IF exists features ;
+DROP VIEW IF exists features;
 CREATE VIEW features as
-SELECT 'Feature' as
+SELECT 'Feature'                                                                                         as
                                                                                                             type,
     id,
     user_id,
@@ -33,3 +38,24 @@ SELECT 'Feature' as
     geometry
 FROM point;
 ALTER VIEW features OWNER TO postgres;
+
+create function public.handle_new_user()
+    returns trigger
+    language plpgsql
+    security definer set search_path = public
+as
+$$
+begin
+    insert into public."user" (id, email, display_name)
+    values (new.id, new.email, new.raw_user_meta_data ->> 'username');
+    return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+    after insert
+    on auth.users
+    for each row
+execute procedure public.handle_new_user();
+
