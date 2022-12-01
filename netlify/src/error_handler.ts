e479c2f5686beb1f2ad1f05ba628fd8fac0bc875
Alherd <algerd.blr@gmail.com>
2022-12-01 13:35:16 +0200

    Sentry error logs

--- netlify/src/error_handler.ts
+++ netlify/src/error_handler.ts
@@ -3,10 +3,11 @@ import "@sentry/tracing";
 
 
 let errorHandler = (error: any) => {
+    console.log(process.env.NODE_ENV)
     console.error(error)
 }
 
-if (process.env.NODE_ENV && process.env.NODE_ENV === 'prod') {
+if (process.env.NODE_ENV && ['prod', 'production'].includes(process.env.NODE_ENV)) {
     Sentry.init({
         dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
         tracesSampleRate: 1.0,
